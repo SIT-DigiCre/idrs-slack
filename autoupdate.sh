d331227ec45c890a27642cd19154f95ac9d86be5
Kaoru Tisen <cordx56@cordx.net>
2018-11-29 12:36:49 +0900

    quote var & exit 0

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,5 +4,7 @@ cd `dirname $0`
 
 dockerfiledata=`cat ./Dockerfile`
 if [ "`git pull`" != "Already up-to-date." ]; then
-	[ "`cat ./Dockerfile`" != $dockerfiledata ] && docker build -t idrs-python:0.1 ./
+	[ "`cat ./Dockerfile`" != "$dockerfiledata" ] && docker build -t idrs-python:0.1 ./
 fi
+
+exit 0
